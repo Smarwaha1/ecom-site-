51b6ab65823c543cbe7dcd2a70ef45034e2e99a0
Saubhagya Marwaha <saubhagyamarwaha@Saubhagyas-MacBook-Air.local>
2023-02-12 22:20:29 +0530

    added more features:

--- shopping/app/views.py
+++ shopping/app/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import  View
 from .models import Customer, Product, OrderPlaced, Cart
+from .forms import CustomerRegistrationForm
 
 # def home(request):
 #  return render(request, 'app/home.html')
@@ -18,8 +20,18 @@ class ProductView(View):
 
 
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+ def get(self, request, pk):
+  """
+  we will be writing detail view over here to get the deatils of the product
+  """
+  product = Product.objects.get(pk=pk)
+  print(product.id)
+  return render(request, 'app/productdetail.html', {'product':product})
+
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -36,17 +48,52 @@ def address(request):
 def orders(request):
  return render(request, 'app/orders.html')
 
-def change_password(request):
- return render(request, 'app/changepassword.html')
+def mobile(request, data=None):
+ if data == None:
+  mobiles = Product.objects.filter(category='M')
+ elif data == 'Apple' or data =='Samsung':
+  mobiles = Product.objects.filter(category='M').filter(brand=data)
+ elif data == 'below':
+  mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=50000)
+ elif data == 'above':
+  mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=50000)
+ return render(request, 'app/mobile.html', {'mobiles':mobiles})
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def topwear(request, data=None):
+ if data == None:
+  topwears = Product.objects.filter(category='TW')
+ elif data == 'hell' or data =='Sau':
+  topwears = Product.objects.filter(category='TW').filter(brand=data)
+ elif data == 'below':
+  topwears = Product.objects.filter(category='TW').filter(discounted_price__lte=450)
+ elif data == 'above':
+  topwears = Product.objects.filter(category='TW').filter(discounted_price__gt=450)
+ return render(request, 'app/topwear.html', {'topwears':topwears})
 
-def login(request):
- return render(request, 'app/login.html')
+def bottomwear(request, data=None):
+ if data == None:
+  bottomwears = Product.objects.filter(category='BW')
+ elif data == 'levis' or data =='hell' or data =='spykar' or data == 'lee':
+  bottomwears = Product.objects.filter(category='BW').filter(brand=data)
+ elif data == 'below':
+  bottomwears = Product.objects.filter(category='BW').filter(discounted_price__lte=560)
+ elif data == 'above':
+  bottomwears = Product.objects.filter(category='BW').filter(discounted_price__gt=560)
+ return render(request, 'app/bottomwear.html', {'bottomwears':bottomwears})
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+
+
+class CustomerRegistrationView(View):
+ def get(self, request):
+  form = CustomerRegistrationForm()
+  return render(request, 'app/customerregistration.html', {'form': form})
+
+ def post(self, request):
+  form = CustomerRegistrationForm(request.POST)
+  if form.is_valid():
+   messages.success(request, 'Congratulations!! Registered Successfully.')
+   form.save()
+  return render(request, 'app/customerregistration.html', {'form': form})
 
 def checkout(request):
  return render(request, 'app/checkout.html')
